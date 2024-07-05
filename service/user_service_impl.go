007a57238b14dfc00d2855adede744e4eca3e6fa
Abuzar Jamshaid <54638297+Abuzar-JS@users.noreply.github.com>
2024-07-05 16:26:44 +0500

    UserService

--- service/user_service_impl.go
+++ service/user_service_impl.go
@@ -1,1 +1,14 @@
 package service
+
+import (
+	"golang-crud/data/request"
+	"golang-crud/data/response"
+)
+
+type UserService interface {
+	Create(user request.CreateUserRequest)
+	Update(user request.UpdateUserRequest)
+	Delete(userId int)
+	FindById(userId int) response.UserResponse
+	FindAll() []response.UserResponse
+}
